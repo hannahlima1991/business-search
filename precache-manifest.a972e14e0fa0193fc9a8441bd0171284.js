1188d879e1471f988741b52ace9a12dfd4777540
Hannah Lima <nanaa_lima@hotmail.com>
2020-09-03 19:43:41 -0500

    Updates

---
+++ precache-manifest.a972e14e0fa0193fc9a8441bd0171284.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9f9e311c465d97d0800f42bc93c27b",
+    "url": "/business-search/index.html"
+  },
+  {
+    "revision": "377ffd423e93a2de5913",
+    "url": "/business-search/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "17f4388df3bc238c2837",
+    "url": "/business-search/static/css/main.79c38a7c.chunk.css"
+  },
+  {
+    "revision": "377ffd423e93a2de5913",
+    "url": "/business-search/static/js/2.afe0d8d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/business-search/static/js/2.afe0d8d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f4388df3bc238c2837",
+    "url": "/business-search/static/js/main.9728ed3f.chunk.js"
+  },
+  {
+    "revision": "574899a3036d22ccd646",
+    "url": "/business-search/static/js/runtime-main.e9d49760.js"
+  },
+  {
+    "revision": "77826e6ec327c9e816bc071b6c582ab4",
+    "url": "/business-search/static/media/backArrow.77826e6e.svg"
+  },
+  {
+    "revision": "dbcca15c4c9054e286a16c647dc53e41",
+    "url": "/business-search/static/media/catGif.dbcca15c.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.6345bf5ff537ef2a7ee96397a095d582.js"
+  "/business-search/precache-manifest.a972e14e0fa0193fc9a8441bd0171284.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/business-search/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
