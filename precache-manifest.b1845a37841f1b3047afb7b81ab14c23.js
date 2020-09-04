ac78980e30d2caaf37cee7d68dc818c27c8eb09f
Hannah Lima <nanaa_lima@hotmail.com>
2020-09-03 19:53:50 -0500

    Updates

---
+++ precache-manifest.b1845a37841f1b3047afb7b81ab14c23.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa8f00fa128e61b53f115994e2547ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "377ffd423e93a2de5913",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "0c5db68cdd08a3274218",
+    "url": "/static/css/main.79c38a7c.chunk.css"
+  },
+  {
+    "revision": "377ffd423e93a2de5913",
+    "url": "/static/js/2.afe0d8d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.afe0d8d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5db68cdd08a3274218",
+    "url": "/static/js/main.4a80f008.chunk.js"
+  },
+  {
+    "revision": "b7016d444710121b9b78",
+    "url": "/static/js/runtime-main.e5cd0b0d.js"
+  },
+  {
+    "revision": "77826e6ec327c9e816bc071b6c582ab4",
+    "url": "/static/media/backArrow.77826e6e.svg"
+  },
+  {
+    "revision": "dbcca15c4c9054e286a16c647dc53e41",
+    "url": "/static/media/catGif.dbcca15c.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/business-search/precache-manifest.df57cf09e429b63bc549b4b5ac9f97b2.js"
+  "/precache-manifest.b1845a37841f1b3047afb7b81ab14c23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/business-search/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
